2f150d7e80b4eefe93cb62afd744c14f7b112a47
Beth Pauley <>
2024-01-24 00:12:42 -0500

    Switch to logo image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,14 @@ const Header = () => {
   return (
     <Navbar expand='lg' className='nav-body' sticky='top'>
       <Container>
-        <Navbar.Brand href='#home'>
-          <h1>Beth Pauley</h1>
+        <Navbar.Brand href='#about'>
+          <img
+            src='src/assets/logo.png'
+            width='20'
+            height='20'
+            className='d-inline-block align-top'
+            alt='Beth Pauley logo'
+          />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
